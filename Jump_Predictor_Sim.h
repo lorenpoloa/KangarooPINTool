9f6dc75d2fb0f0f15de649d16ee65533d78de45f
Lorenzo Polo <73763091+lorenpoloa@users.noreply.github.com>
2025-05-15 14:41:21 +0200

    Create Jump_Predictor_Sim.h

---
+++ Jump_Predictor_Sim.h
@@ -0,0 +1,31 @@
+#ifndef JUMP_PREDICTOR_SIM_H
+#define JUMP_PREDICTOR_SIM_H
+
+#include "pin.H"
+#include <map>
+
+enum PredictorType {
+    ALWAYS_TAKEN,
+    ALWAYS_NOT_TAKEN,
+    ONE_BIT,
+    TWO_BIT,
+    NUM_PREDICTORS
+};
+
+struct PredictorStats {
+    UINT64 correct = 0;
+    UINT64 total = 0;
+};
+
+class BranchPredictor {
+public:
+    void Update(ADDRINT addr, BOOL actualTaken);
+    const PredictorStats* GetStats() const;
+
+private:
+    std::map<ADDRINT, bool> oneBitTable;
+    std::map<ADDRINT, UINT8> twoBitTable;
+    PredictorStats stats[NUM_PREDICTORS];
+};
+
+#endif // JUMP_PREDICTOR_SIM_H
